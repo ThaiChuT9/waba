3b83d9de7faa8f9c8d273db6c532a9e1bb2c0744
ThaiChuT9 <iamtk4i@gmail.com>
2024-06-20 17:48:55 +0700

    testy-real

---
+++ my-testy/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Pricing from './component/Pricing';
+
+function App() {
+  return (
+    <div className="bg-gradient-to-br from-blue-900 to-blue-300">
+      <Pricing/>
+    </div>
+  );
+}
+
+export default App;
