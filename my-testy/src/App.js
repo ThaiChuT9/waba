97bbc9eb0340a1aec87ce7955227c7317028baed
ThaiChuT9 <iamtk4i@gmail.com>
2024-06-25 21:54:00 +0700

    a

--- my-testy/src/App.js
+++ my-testy/src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import Pricing from './component/Pricing';
 import Productlist from './component/Productlist';
 import Paint from './component/paint';
+import {BrowseRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="">
-      <Pricing/>
-      <Productlist/>
-      <Paint/>
-    </div>
-  );
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Productlist}/>
+      <Route path="/pricing" component={Pricing}/>
+      <Route path="/paint" component={Paint}/>
+    </Switch>
+  </Router>
 }
 
 export default App;
